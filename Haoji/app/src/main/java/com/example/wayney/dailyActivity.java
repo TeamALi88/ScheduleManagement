5edc4d5ae3582888223ba09a0b0063c0a9c80e05
a270331257 <31378008+a270331257@users.noreply.github.com>
2017-11-01 20:41:11 +0800

    整体模型绘制

---
+++ Haoji/app/src/main/java/com/example/wayney/dailyActivity.java
@@ -0,0 +1,13 @@
+package com.example.wayney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dailyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daily);
+    }
+}
---
+++ Haoji/app/src/main/java/com/example/wayney/userActivity.java
@@ -0,0 +1,13 @@
+package com.example.wayney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class userActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infoshow);
+    }
+}
