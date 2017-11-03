0e8982d840de2c1e2daa1d842cf781459a858ae4
a270331257 <31378008+a270331257@users.noreply.github.com>
2017-11-03 14:25:08 +0800

    加入返回键

--- Haoji/app/src/main/java/com/example/wayney/userActivity.java
+++ Haoji/app/src/main/java/com/example/wayney/userActivity.java
@@ -2,6 +2,8 @@ package com.example.wayney;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class userActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class userActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.infoshow);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
