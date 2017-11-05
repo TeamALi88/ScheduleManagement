e2695bdd86de19ced6f59cbe345eda923ebc3154
a270331257 <31378008+a270331257@users.noreply.github.com>
2017-11-05 23:36:48 +0800

    ConflictSolve

--- Haoji/app/src/main/java/com/example/wayney/forgetPasswordActivity.java
+++ Haoji/app/src/main/java/com/example/wayney/forgetPasswordActivity.java
@@ -1,4 +1,4 @@
-package com.example.schedule;
+package com.example.wayney;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
--- Haoji/app/src/main/java/com/example/wayney/login1Activity.java
+++ Haoji/app/src/main/java/com/example/wayney/login1Activity.java
@@ -1,10 +1,11 @@
-package com.example.schedule;
+package com.example.wayney;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class login1Activity extends AppCompatActivity {
@@ -16,6 +17,20 @@ public class login1Activity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
