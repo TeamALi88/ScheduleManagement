f225fbb457fc328c0cbc8f1faf28d1115812dd29
lijiaqun <31312201+lijiaqun@users.noreply.github.com>
2017-11-06 23:04:52 +0800

    用户界面跳转

--- Haoji/app/src/main/java/com/example/wayney/changePasswordActivity.java
+++ Haoji/app/src/main/java/com/example/wayney/changePasswordActivity.java
@@ -1,11 +1,14 @@
 package com.example.wayney;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class changePasswordActivity extends AppCompatActivity {
 
@@ -15,6 +18,28 @@ public class changePasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_change_password);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        Button edit_psw = (Button) findViewById(R.id.edit_psw);
+        edit_psw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(changePasswordActivity.this,passChangeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- Haoji/app/src/main/java/com/example/wayney/showinfoActivity.java
+++ Haoji/app/src/main/java/com/example/wayney/showinfoActivity.java
@@ -1,10 +1,12 @@
 package com.example.wayney;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class showinfoActivity extends AppCompatActivity {
@@ -15,8 +17,29 @@ public class showinfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_showinfo);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
+        FloatingActionButton edit_info=(FloatingActionButton) findViewById(R.id.edit_info);
+        edit_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(showinfoActivity.this,changePasswordActivity.class);
+                startActivity(intent);
+            }
+        });
 
+    }
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
