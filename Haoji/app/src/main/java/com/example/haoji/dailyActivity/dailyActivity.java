aa79e776a552107252f35c54eb5eccb361f8484f
a270331257 <31378008+a270331257@users.noreply.github.com>
2017-11-13 21:28:19 +0800

    实现多级悬浮按钮

--- Haoji/app/src/main/java/com/example/haoji/dailyActivity/dailyActivity.java
+++ Haoji/app/src/main/java/com/example/haoji/dailyActivity/dailyActivity.java
@@ -11,10 +11,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.Toast;
+
 import com.example.haoji.Button.DragFloatActionButton;
 
 import com.example.haoji.R;
 import com.example.haoji.userActivity.login1Activity;
+import com.example.haoji.Button.SectorMenuButton;
+import com.example.haoji.Button.ButtonData;
+import com.example.haoji.Button.ButtonEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class dailyActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -25,15 +33,16 @@ public class dailyActivity extends AppCompatActivity
         setContentView(R.layout.sidebar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        initBottomSectorMenuButton();
 
-        DragFloatActionButton addSchedule = (DragFloatActionButton) findViewById(R.id.addSchedule);
-        addSchedule.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(dailyActivity.this ,newPlan.class);
-                startActivity(intent);
-            }
-        });
+        //DragFloatActionButton addSchedule = (DragFloatActionButton) findViewById(R.id.addSchedule);
+        //addSchedule.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View view) {
+               // Intent intent = new Intent(dailyActivity.this ,newPlan.class);
+                //startActivity(intent);
+           // }
+       // });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -43,6 +52,46 @@ public class dailyActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    private void initBottomSectorMenuButton() {
+        SectorMenuButton sectorMenuButton = (SectorMenuButton) findViewById(R.id.bottom_sector_menu);
+        final List<ButtonData> buttonDatas = new ArrayList<>();
+        int[] drawable = {R.mipmap.add, R.mipmap.speak,
+                R.mipmap.dynamic, R.mipmap.text};
+        for (int i = 0; i < 4; i++) {
+            ButtonData buttonData = ButtonData.buildIconButton(this, drawable[i], 0);
+            buttonData.setBackgroundColorId(this, R.color.colorAccent);
+            buttonDatas.add(buttonData);
+        }
+        sectorMenuButton.setButtonDatas(buttonDatas);
+        setListener(sectorMenuButton);
+    }
+
+    private void setListener(final SectorMenuButton button) {
+        button.setButtonEventListener(new ButtonEventListener() {
+            @Override
+            public void onButtonClicked(int index) {
+                int buttonid = index;
+                if (buttonid == 3) {
+                    Intent intent = new Intent(dailyActivity.this ,newPlan.class);
+                        startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onExpand() {
+            }
+
+            @Override
+            public void onCollapse() {
+            }
+        });
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(dailyActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 
     @Override
