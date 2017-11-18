47fcff62cbca371ed9b5ada3ef2343f73e0690fe
zyccccla <31388106+zyccccla@users.noreply.github.com>
2017-11-19 00:00:13 +0800

    登录接口对接成功

--- Haoji/app/src/main/java/com/example/haoji/userActivity/login1Activity.java
+++ Haoji/app/src/main/java/com/example/haoji/userActivity/login1Activity.java
@@ -1,45 +1,42 @@
 package com.example.haoji.userActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+//import android.telecom.Call;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.haoji.Button.ButtonData;
+import com.example.haoji.HttpUtil;
 import com.example.haoji.R;
-import com.tencent.connect.UserInfo;
-import com.tencent.connect.auth.QQToken;
-import com.tencent.connect.common.Constants;
-import com.tencent.tauth.IUiListener;
-import com.tencent.tauth.Tencent;
-import com.tencent.tauth.UiError;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 
-public class login1Activity extends AppCompatActivity implements View.OnClickListener{
+import okhttp3.Call;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText editText1;
     private EditText editText2;
-    private static final String TAG = "login1Activity";
-    private static final String APP_ID = "1105602574";//官方获取的APPID
-    private Tencent mTencent;
-    private BaseUiListener mIUiListener;
-    private UserInfo mUserInfo;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login1);
+        setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,127 +45,76 @@ public class login1Activity extends AppCompatActivity implements View.OnClickLis
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-        TextView forget_psw = (TextView) findViewById(R.id.forget_psw);
-        forget_psw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login1Activity.this,forgetPasswordActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        TextView register = (TextView) findViewById(R.id.register);
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login1Activity.this,RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button button = (Button) findViewById(R.id.login);
-        editText1 = (EditText) findViewById(R.id.userName);
-        editText2 = (EditText) findViewById(R.id.passWord);
+        editText1 = (EditText) findViewById(R.id.userphone);
+        editText2 = (EditText) findViewById(R.id.password);
+        Button button = (Button) findViewById(R.id.modifyPsw);
         button.setOnClickListener(this);
-
-        mTencent = Tencent.createInstance(APP_ID,login1Activity.this.getApplicationContext());
     }
-
-    public boolean onOptionsItemSelected (MenuItem item) {
-            switch (item.getItemId()) {
-                case android.R.id.home:
-                    this.finish(); // back button
-                    return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
-
     public void onClick(View v){
         switch (v.getId()) {
-            case R.id.login:
-                String text_userName = editText1.getText().toString();
-                String text_passWord = editText2.getText().toString();
-                break;
-            default :
-                break;
-        }
-    }
+            case R.id.modifyPsw:
+                //获取用户名和密码
+                String inputText1 = editText1.getText().toString();
+                String inputText2 = editText2.getText().toString();
+                //构造JSONObject类型的数据
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("username",inputText1);
+                    jsonObject.put("password",inputText2);
+                    jsonObject.put("phonenum",inputText1);
+                    jsonObject.put("qq"," ");
+                    jsonObject.put("weibo"," ");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                //发送请求
+                HttpUtil.sendOkHttpRequest("http://97.64.21.155:8001/user/signup", jsonObject, new okhttp3.Callback() {
+                    public void onResponse(okhttp3.Call call, Response response) throws IOException {
+                        String responseData = response.body().string();
+                        if(judgeState(responseData)){
 
-    public void buttonLogin(View v){
-        /**通过这句代码，SDK实现了QQ的登录，这个方法有三个参数，第一个参数是context上下文，第二个参数SCOPO 是一个String类型的字符串，表示一些权限
-         官方文档中的说明：应用需要获得哪些API的权限，由“，”分隔。例如：SCOPE = “get_user_info,add_t”；所有权限用“all”
-         第三个参数，是一个事件监听器，IUiListener接口的实例，这里用的是该接口的实现类 */
-        mIUiListener = new BaseUiListener();
-        //all表示获取所有权限
-        mTencent.login(login1Activity.this,"all", mIUiListener);
-    }
-
-    private class BaseUiListener implements IUiListener{
-
-        @Override
-        public void onComplete(Object response) {
-            Toast.makeText(login1Activity.this, "授权成功", Toast.LENGTH_SHORT).show();
-            Log.e(TAG, "response:" + response);
-            JSONObject obj = (JSONObject) response;
-            try {
-                String openID = obj.getString("openid");
-                String accessToken = obj.getString("access_token");
-                String expires = obj.getString("expires_in");
-                mTencent.setOpenId(openID);
-                mTencent.setAccessToken(accessToken,expires);
-                QQToken qqToken = mTencent.getQQToken();
-                mUserInfo = new UserInfo(getApplicationContext(),qqToken);
-                mUserInfo.getUserInfo(new IUiListener() {
-                    @Override
-                    public void onComplete(Object response) {
-                        Log.e(TAG,"登录成功"+response.toString());
+                        }
+                        else{
+                            Toast.makeText(RegisterActivity.this,"注册失败",Toast.LENGTH_LONG);
+                        }
                     }
-
-                    @Override
-                    public void onError(UiError uiError) {
-                        Log.e(TAG,"登录失败"+uiError.toString());
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        Log.e(TAG,"登录取消");
-
+                    public void onFailure(okhttp3.Call call, IOException e) {
+                        e.printStackTrace();
                     }
                 });
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+                break;
+            default:
+                break;
         }
-
-        @Override
-        public void onError(UiError uiError) {
-            Toast.makeText(login1Activity.this, "授权失败", Toast.LENGTH_SHORT).show();
-
-        }
-
-        @Override
-        public void onCancel() {
-            Toast.makeText(login1Activity.this, "授权取消", Toast.LENGTH_SHORT).show();
-
-        }
-
     }
-    /**
-     * 在调用Login的Activity或者Fragment中重写onActivityResult方法
-     * @param requestCode
-     * @param resultCode
-     * @param data
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == Constants.REQUEST_LOGIN){
-            Tencent.onActivityResultData(requestCode,resultCode,data,mIUiListener);
+
+    private boolean judgeState(String responseData) {
+        try{
+            JSONArray jsonArray = new JSONArray(responseData);
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            int state = jsonObject.getInt("state");
+            if(state==200) return true;
+            else return false;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-        super.onActivityResult(requestCode, resultCode, data);
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+      switch(item.getItemId()){
+          case android.R.id.home:
+              this.finish();
+              return true;
+      }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
 }
-
-
