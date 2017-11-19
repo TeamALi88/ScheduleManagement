4fc0f8d6bf1422b58b40f5e6920bfda93a5cdbb9
zyccccla <31388106+zyccccla@users.noreply.github.com>
2017-11-20 00:19:05 +0800

    update the login1Activity

--- Haoji/app/src/main/java/com/example/haoji/userActivity/login1Activity.java
+++ Haoji/app/src/main/java/com/example/haoji/userActivity/login1Activity.java
@@ -1,5 +1,6 @@
 package com.example.haoji.userActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,11 +13,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.haoji.Button.ButtonData;
 import com.example.haoji.HttpUtil;
 import com.example.haoji.R;
+import com.example.haoji.dailyActivity.dailyActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,76 +33,115 @@ import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
-    private EditText editText1;
-    private EditText editText2;
+public class login1Activity extends AppCompatActivity{
+//    private EditText editText1;
+//    private EditText editText2;
+    private EditText userPhone;//用户名
+    private EditText passWord;//密码
+    private Button loginBtn;//登录按钮
+    private TextView register;//注册控件
+    private TextView passForget;//忘记密码
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //通过id获取各控件
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        userPhone = (EditText) findViewById(R.id.userphone);
+        passWord = (EditText) findViewById(R.id.password);
+        loginBtn = (Button) findViewById(R.id.login);
+        register = (TextView) findViewById(R.id.register);
+        passForget = (TextView) findViewById(R.id.forget_psw);
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-        editText1 = (EditText) findViewById(R.id.userphone);
-        editText2 = (EditText) findViewById(R.id.password);
-        Button button = (Button) findViewById(R.id.modifyPsw);
-        button.setOnClickListener(this);
-    }
-    public void onClick(View v){
-        switch (v.getId()) {
-            case R.id.modifyPsw:
-                //获取用户名和密码
-                String inputText1 = editText1.getText().toString();
-                String inputText2 = editText2.getText().toString();
-                //构造JSONObject类型的数据
+
+        //登录按钮的点击事件
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //获取手机号和密码
+                String myPhone = userPhone.getText().toString();
+                String myPass = passWord.getText().toString();
+                //构造发送的json数据
                 JSONObject jsonObject = new JSONObject();
                 try {
-                    jsonObject.put("username",inputText1);
-                    jsonObject.put("password",inputText2);
-                    jsonObject.put("phonenum",inputText1);
-                    jsonObject.put("qq"," ");
-                    jsonObject.put("weibo"," ");
+                    jsonObject.put("password",myPass);
+                    jsonObject.put("phonenum",myPhone);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                //发送请求
-                HttpUtil.sendOkHttpRequest("http://97.64.21.155:8001/user/signup", jsonObject, new okhttp3.Callback() {
+                //发送okhttp 请求
+                HttpUtil.sendOkHttpRequest("http://97.64.21.155:8001/user/login", jsonObject, new okhttp3.Callback() {
                     public void onResponse(okhttp3.Call call, Response response) throws IOException {
                         String responseData = response.body().string();
-                        if(judgeState(responseData)){
-
-                        }
-                        else{
-                            Toast.makeText(RegisterActivity.this,"注册失败",Toast.LENGTH_LONG);
+                        if(isValid()){
+                            if(judgeState(responseData)){       //跳转到主界面
+                                Intent intent=new Intent(login1Activity.this,dailyActivity.class);
+                                startActivity(intent);
+                            }
+                            else{
+                                Toast.makeText(login1Activity.this,"登录失败",Toast.LENGTH_LONG);
+                            }
                         }
                     }
                     public void onFailure(okhttp3.Call call, IOException e) {
                         e.printStackTrace();
                     }
                 });
-                break;
-            default:
-                break;
-        }
+            }
+        });
+        //跳转到注册界面
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(login1Activity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        //跳转到忘记密码界面
+        passForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(login1Activity.this,forgetPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    //判断请求返回状态
     private boolean judgeState(String responseData) {
         try{
-            JSONArray jsonArray = new JSONArray(responseData);
-            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            JSONObject jsonObject = new JSONObject(responseData);
             int state = jsonObject.getInt("state");
-            if(state==200) return true;
+            if(state==200) {
+                return true;
+            }
+            else if(state==401){
+                Toast.makeText(this,"手机号错误",Toast.LENGTH_SHORT);
+                return false;
+            }
+            else if(state==402){
+                Toast.makeText(this,"密码错误",Toast.LENGTH_SHORT);
+                return false;
+            }
             else return false;
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return false;
+    }
+    //判断用户名和密码是否合法
+    public boolean isValid(){
+        if (userPhone.getText().toString().trim().equals("")||passWord.getText().toString().trim().equals("")) {
+            Toast.makeText(this,"用户名或密码有误",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
     @Override
