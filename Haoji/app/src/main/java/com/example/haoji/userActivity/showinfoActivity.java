81a54ab1cca5a9d9864efe459150001f22feb68e
a270331257 <31378008+a270331257@users.noreply.github.com>
2017-11-26 10:22:31 +0800

    增加注销按钮、修改返回逻辑

--- Haoji/app/src/main/java/com/example/haoji/userActivity/showinfoActivity.java
+++ Haoji/app/src/main/java/com/example/haoji/userActivity/showinfoActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +26,7 @@ public class showinfoActivity extends AppCompatActivity {
         TextView userPhone = (TextView)findViewById(R.id.text_view6);
         TextView qqNum = (TextView)findViewById(R.id.text_view7);
         TextView wbNum = (TextView)findViewById(R.id.text_view8);
+        Button backLogin = (Button)findViewById(R.id.backLogin);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
@@ -50,7 +52,14 @@ public class showinfoActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        backLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                app.setState(0);
+                Intent intent = new Intent(showinfoActivity.this,login1Activity.class);
+                startActivity(intent);
+            }
+        });
     }
     public boolean onOptionsItemSelected (MenuItem item) {
         switch (item.getItemId()) {
