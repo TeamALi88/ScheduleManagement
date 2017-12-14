739c761dccbf7fd058de4e74d542971d67882cbe
Sil <36268198@qq.com>
2017-12-14 21:43:59 +0800

    String

--- Haoji/app/src/main/java/com/example/haoji/dailyActivity/dailyActivity.java
+++ Haoji/app/src/main/java/com/example/haoji/dailyActivity/dailyActivity.java
@@ -133,7 +133,14 @@ public class dailyActivity extends AppCompatActivity
                 int buttonid = index;
                 if (buttonid == 3) {
                     Intent intent = new Intent(dailyActivity.this ,newPlan.class);
+                //    intent.putExtra("from", "Main");调用的时候要把"Main"改成其他的就行
+                //    String test="啊啊啊啊2017年05月10日12时18分啊啊啊啊";严格按照这个格式，前导零不能没有且年份长度为4，其他长度为2
+                //    intent.putExtra("txt",test);//通过这个传递数据
                     startActivity(intent);
+                }
+                //TODO 调用语音识别,语音识别调用newPlan
+                if (buttonid == 1){
+
                 }
             }
 
