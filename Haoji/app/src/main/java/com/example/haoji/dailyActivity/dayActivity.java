44498f6a1a2cda45c3e6bde4cfa8e382014bf6d9
15659970389 <daxuebianchengjy@163.com>
2017-12-16 09:14:15 +0800

    主界面修改

---
+++ Haoji/app/src/main/java/com/example/haoji/dailyActivity/dayActivity.java
@@ -0,0 +1,15 @@
+package com.example.haoji.dailyActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.haoji.R;
+
+public class dayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day);
+    }
+}
